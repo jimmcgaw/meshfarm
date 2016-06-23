98576421d8ff9ce145f2e3f8e86efa75e736c639
jimmcgaw <jpmcgaw@gmail.com>
2016-06-23 12:25:46 -0700

    encapsulate draw in simple square class

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,34 @@
 (function(){
-  
+  var Square = function(length, x, y, borderColor, fillColor){
+    this.length = length;
+    this.x = x;
+    this.y = y;
+    this.borderColor = borderColor;
+    this.fillColor = fillColor;
+  };
+
+  Square.prototype.setPosition = function(x, y){
+    this.x = x;
+    this.y = y;
+  };
+
+  Square.prototype.draw = function(context){
+    context.strokeStyle = this.borderColor;
+    context.fillStyle = this.fillColor;
+    var centerX = this.x - (this.length / 2);
+    var centerY = this.y - (this.length / 2);
+    context.fillRect(centerX, centerY, this.length, this.length);
+    context.strokeRect(centerX, centerY, this.length, this.length);
+  };
+
+  var canvas = document.getElementById('canvas');
+  if (canvas.getContext){
+    var context = canvas.getContext('2d');
+    var square = new Square(10, 50, 50, '#333', '#ccc');
+    square.draw(context);
+    square.setPosition(100, 100);
+    square.draw(context);
+    square.setPosition(200, 100);
+    square.draw(context);
+  }
 }());
